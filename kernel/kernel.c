1ecde05a62aff6a3824760ee116b9675aad3dd72
josem1942 <48543854+josem1942@users.noreply.github.com>
2026-08-20 10:19:58 -0500

    Quest 08

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -35,7 +35,7 @@ void kernel_main() {
 	if (fb_init() != 0) BUG();          // will show the OS logo
 
 	// test_ktimer();
-	// test_fb_voffset();               // cycle through color quads
+	test_fb_voffset();               // cycle through color quads
 // 	donut();		// !STUDENT_DONOT_SEE    uses virtual timer for animation
 	/* STUDENT_TODO: your code here */
 
